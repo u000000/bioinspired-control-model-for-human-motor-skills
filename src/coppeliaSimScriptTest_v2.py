f86f8520b25daa1224a5aae273f80a27bd6d6db7
u000000 <adasko99a@gmail.com>
2024-04-17 11:26:52 +0200

    works!

--- src/coppeliaSimScriptTest_v2.py
+++ src/coppeliaSimScriptTest_v2.py
@@ -8,6 +8,12 @@ def sysCall_init():
     self.joint2 = sim.getObject('/joint2')
     self.jointAngles = []
     self.i = 0
+    self.waitUntil = 0
+    
+    self.reachedTarget1 = 0
+    self.reachedTarget2 = 0
+    self.reachedTargetPrev1 = 0
+    self.reachedTargetPrev2 = 0
 
     with open('/home/u000000/2_Semester/bioinspired-control-model-for-human-motor-skills/src/jointAngles.csv', 'r') as file:
         for line in file:
@@ -16,20 +22,26 @@ def sysCall_init():
             posJoint2 = math.radians(float(pos[1]))
             self.jointAngles.append((posJoint1, posJoint2))
 
+    print('jointAngles = ' + str(self.jointAngles))
+
+    self.reachedTargetPrev1 = self.jointAngles[0][0]
+    self.reachedTarget1 = self.jointAngles[0][0]
+    self.reachedTargetPrev2 = self.jointAngles[0][1]
+    self.reachedTarget2 = self.jointAngles[0][1]
 
 def sysCall_actuation():
-    # put your actuation code here
-    # theta1 = sim.getJointPosition(self.joint1)
-    # theta2 = sim.getJointPosition(self.joint2)
-    # print('theta1 = ' + str(theta1) + ' theta2 = ' + str(theta2))
 
     # moving accordingly to theta values
     pos = self.jointAngles[self.i]
+    print('pos = ' + str((pos)))
     sim.setJointTargetPosition(self.joint1, pos[0])
     sim.setJointTargetPosition(self.joint2, pos[1])
-    self.i += 1
-
-    pass
+    tolerance = 0.25*math.pi/180
+    if [abs(sim.getJointPosition(self.joint1)-sim.getJointTargetPosition(self.joint1)) <= tolerance and abs(sim.getJointPosition(self.joint2)-sim.getJointTargetPosition(self.joint2)) <= tolerance]:
+        if self.i < len(self.jointAngles):
+            self.i += 1
+    else:
+        return
 
 def sysCall_sensing():
     # put your sensing code here
---
+++ src/coppeliaSimScriptTest_v3.py
@@ -0,0 +1,77 @@
+#python
+import math
+
+def sysCall_init():
+    sim = require('sim')
+
+    self.joint1 = sim.getObject('/joint1')
+    self.joint2 = sim.getObject('/joint2')
+    self.jointAngles = []
+    self.i = 0
+    self.waitUntil = 0
+
+    self.reachedTarget1 = 0
+    self.reachedTarget2 = 0
+    self.reachedTargetPrev1 = 0
+    self.reachedTargetPrev2 = 0
+
+    with open('/home/u000000/2_Semester/bioinspired-control-model-for-human-motor-skills/src/jointAngles.csv', 'r') as file:
+        for line in file:
+            pos = line.split(',')
+            posJoint1 = math.radians(float(pos[0]))
+            posJoint2 = math.radians(float(pos[1]))
+            self.jointAngles.append((posJoint1, posJoint2))
+
+    print('jointAngles = ' + str(self.jointAngles))
+
+    self.reachedTargetPrev1 = self.jointAngles[0][0]
+    self.reachedTarget1 = self.jointAngles[0][0]
+    self.reachedTargetPrev2 = self.jointAngles[0][0]
+    self.reachedTarget2 = self.jointAngles[0][0]
+
+    print('reachedTargetPrev1 = ' + str(self.reachedTargetPrev1))
+    print('reachedTarget1 = ' + str(self.reachedTarget1))
+    print('reachedTargetPrev2 = ' + str(self.reachedTargetPrev2))
+    print('reachedTarget2 = ' + str(self.reachedTarget2))
+
+def sysCall_actuation():
+    # put your actuation code here
+    # if self.waitUntil > sim.getSimulationTime():
+    #     print(self.waitUntil)
+    #     return
+
+    tolerance = math.radians(1)
+
+    self.reachedTargetPrev1 = self.reachedTarget1
+    self.reachedTarget1 = abs(sim.getJointPosition(self.joint1)-self.jointAngles[self.i][0]) <= tolerance
+    # print('reachedTarget1 = ' + str(self.reachedTarget1))
+    
+    self.reachedTargetPrev2 = self.reachedTarget2
+    self.reachedTarget2 = abs(sim.getJointPosition(self.joint2)-self.jointAngles[self.i][0]) <= tolerance
+    # print('reachedTarget2 = ' + str(self.reachedTarget2))
+    
+    if not self.reachedTarget1 and not self.reachedTarget2:
+        # target not reached: do nothing
+        sim.setJointTargetPosition(self.joint1, self.jointAngles[self.i][0])
+        sim.setJointTargetPosition(self.joint2, self.jointAngles[self.i][1])
+        pass
+    elif not self.reachedTargetPrev1 and not self.reachedTargetPrev2:
+        # target just reached, wait 1s
+        self.waitUntil = sim.getSimulationTime() + 1
+    else:
+        # target reached, wait done, select next target
+        if self.i < len(self.jointAngles)-1:
+            self.i += 1
+            sim.setJointTargetPosition(self.joint1, self.jointAngles[self.i][0])
+            sim.setJointTargetPosition(self.joint2, self.jointAngles[self.i][1])
+
+def sysCall_sensing():
+    # put your sensing code here
+
+    pass
+
+def sysCall_cleanup():
+    # do some clean-up here
+    pass
+
+# See the user manual or the available code snippets for additional callback functions and details
